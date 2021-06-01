3aa97457d656cc844795a87c075e5b8cc36fbb9b
kiang <kiange@gmail.com>
2021-06-01 22:13:36 +0800

    add rate to confirmed data

--- scripts/01_fetch_od.php
+++ scripts/01_fetch_od.php
@@ -16,24 +16,24 @@ file_put_contents($dailyFile, file_get_contents('https://od.cdc.gov.tw/eic/Day_C
 $fh = fopen($specimenFile, 'r');
 $head = fgetcsv($fh, 2048);
 $specimen = [];
-while($line = fgetcsv($fh, 2048)) {
+while ($line = fgetcsv($fh, 2048)) {
     $data = array_combine($head, $line);
     $time = strtotime($data['通報日']);
     $y = date('Y', $time);
-    if(!isset($specimen[$y])) {
+    if (!isset($specimen[$y])) {
         $specimen[$y] = [];
     }
     array_shift($data);
-    foreach($data AS $k => $v) {
+    foreach ($data as $k => $v) {
         $data[$k] = intval($v);
     }
     $specimen[$y][date('md', $time)] = array_values($data);
 }
 $specimenPath = $dataPath . '/specimen';
-if(!file_exists($specimenPath)) {
+if (!file_exists($specimenPath)) {
     mkdir($specimenPath, 0777);
 }
-foreach($specimen AS $y => $data1) {
+foreach ($specimen as $y => $data1) {
     ksort($data1);
     file_put_contents($specimenPath . '/' . $y . '.json', json_encode($data1, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 }
@@ -63,22 +63,31 @@ $townTemplate = [
     ],
     'age' => [],
 ];
-for($i = $timeBegin; $i < $timeEnd; $i+= 86400) {
+for ($i = $timeBegin; $i < $timeEnd; $i += 86400) {
     $townTemplate['days'][date('Ymd', $i)] = 0;
 }
 
 $confirmed = [];
 $now = date('Y-m-d H:i:s', $timeEnd);
 $towns = [];
+$rateBase = [];
 while ($line = fgetcsv($fh, 2048)) {
     $data = array_combine($head, $line);
     if ($data['是否為境外移入'] === '否') {
         $y = substr($data['個案研判日'], 0, 4);
         if (!isset($confirmed[$y])) {
-            if($y == date('Y')) {
-                $day = date('Ymd', strtotime('-1 day'));
+            if ($y == date('Y')) {
+                $dayTime = strtotime('-1 day');
             } else {
-                $day = date('Ymd', strtotime($y . '-12-31'));
+                $dayTime = strtotime($y . '-12-31');
+            }
+            $day = date('Ymd', $dayTime);
+            $rateBaseDay = date('Ymd', strtotime('-1 day', $dayTime));
+            $rateBaseFile = $pathConfirmed . '/' . $rateBaseDay . '.json';
+            if(file_exists($rateBaseFile)) {
+                $rateBase[$y] = json_decode(file_get_contents($pathConfirmed . '/' . $rateBaseDay . '.json'), true);
+            } else {
+                $rateBase[$y] = [];
             }
             $confirmed[$y] = [
                 'meta' => [
@@ -87,61 +96,75 @@ while ($line = fgetcsv($fh, 2048)) {
                     'modified' => $now,
                 ],
                 'data' => [],
+                'rate' => [],
             ];
         }
         $confirmed[$y]['meta']['total'] += $data['確定病例數'];
-        if(!isset($confirmed[$y]['data'][$data['縣市']])) {
+        if (!isset($confirmed[$y]['data'][$data['縣市']])) {
             $confirmed[$y]['data'][$data['縣市']] = [];
+            $confirmed[$y]['rate'][$data['縣市']] = [];
         }
-        if(!isset($confirmed[$y]['data'][$data['縣市']][$data['鄉鎮']])) {
+        if (!isset($confirmed[$y]['data'][$data['縣市']][$data['鄉鎮']])) {
             $confirmed[$y]['data'][$data['縣市']][$data['鄉鎮']] = 0;
+            $confirmed[$y]['rate'][$data['縣市']][$data['鄉鎮']] = 0.0;
         }
         $confirmed[$y]['data'][$data['縣市']][$data['鄉鎮']] += $data['確定病例數'];
+        if(isset($rateBase[$y]['data'][$data['縣市']][$data['鄉鎮']])) {
+            $confirmed[$y]['rate'][$data['縣市']][$data['鄉鎮']] = round(
+                ($confirmed[$y]['data'][$data['縣市']][$data['鄉鎮']] - $rateBase[$y]['data'][$data['縣市']][$data['鄉鎮']])
+                    / $rateBase[$y]['data'][$data['縣市']][$data['鄉鎮']],
+                1
+            );
+        } else {
+            $confirmed[$y]['rate'][$data['縣市']][$data['鄉鎮']] = 100.0;
+        }
+        
 
         $townKey = $data['縣市'] . $data['鄉鎮'];
-        if(!isset($towns[$townKey])) {
+        if (!isset($towns[$townKey])) {
             $towns[$townKey] = $townTemplate;
         }
-        if(isset($towns[$townKey]['days'][$data['個案研判日']])) {
+        if (isset($towns[$townKey]['days'][$data['個案研判日']])) {
             $towns[$townKey]['days'][$data['個案研判日']] += $data['確定病例數'];
         }
-        if($data['性別'] === '女') {
+        if ($data['性別'] === '女') {
             $towns[$townKey]['gender']['f'] += $data['確定病例數'];
         } else {
             $towns[$townKey]['gender']['m'] += $data['確定病例數'];
         }
-        if(!isset($towns[$townKey]['age'][$data['年齡層']])) {
+        if (!isset($towns[$townKey]['age'][$data['年齡層']])) {
             $towns[$townKey]['age'][$data['年齡層']] = 0;
         }
         $towns[$townKey]['age'][$data['年齡層']] += $data['確定病例數'];
     }
 }
-foreach($confirmed AS $y => $data1) {
+foreach ($confirmed as $y => $data1) {
     ksort($confirmed[$y]['data']);
-    foreach($confirmed[$y]['data'] AS $city => $data2) {
+    foreach ($confirmed[$y]['data'] as $city => $data2) {
         ksort($confirmed[$y]['data'][$city]);
     }
     $targetFile = $pathConfirmed . '/' . $y . '.json';
     $fileToWrite = true;
-    if(file_exists($targetFile)) {
+    if (file_exists($targetFile)) {
         $json = json_decode(file_get_contents($targetFile), true);
-        if($json['data'] === $confirmed[$y]['data']) {
+        if ($json['data'] === $confirmed[$y]['data']) {
             $fileToWrite = false;
         }
     }
-    if($fileToWrite) {
+    if ($fileToWrite) {
         file_put_contents($targetFile, json_encode($confirmed[$y], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
         file_put_contents($pathConfirmed . '/' . $day . '.json', json_encode($confirmed[$y], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
     }
 }
 
-function cmp($a, $b) {
+function cmp($a, $b)
+{
     $a = intval($a);
     $b = intval($b);
     return ($a > $b);
 }
 
-foreach($towns AS $k => $data) {
+foreach ($towns as $k => $data) {
     uksort($data['age'], 'cmp');
     file_put_contents($pathTown . '/' . $k . '.json', json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 }
